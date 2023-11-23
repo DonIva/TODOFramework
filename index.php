db7a08265a5585528dfc2878267e7e32d57fa059
DonIva <ernesto.arreagac@gmail.com>
2023-11-23 18:19:06 -0500

    Se agrega el proyecto

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+// TODO Framework versión 1.0.0
+// Desarrollado por TODO Mining
+// Noviembre 2023
+
+// Requerir el archivo de la clase Bee.php
+require_once 'app/classes/Todo.php';
+
+// Ejecutar el framework TODO
+Todo::be();
+
---
+++ templates/views/home/todoView.php
@@ -0,0 +1,25 @@
+<?php require_once INCLUDES.'inc_header.php'; ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-6 text-center offset-md-3">
+      <img src="<?php echo IMAGES.'Logo_TODO_Mining.png' ?>" alt="TODO Framework" class="img-fluid" style="width: 200px;">
+      <h2 class="text-white mt-5 mb-3"><span class="text-warning">TODO</span> Framework</h2>
+      <p class="text-center text-white">El mejor de los Frameworks para tus proyectos personales o profesionales.</p>
+      <ul class="text-white">
+        <li>Desarrollado con PHP, Javascript y HTML5</li>
+        <li>Utilizando patrón <b>MVC</b></li>
+      </ul>
+      <div class="mt-5">
+        <a class="btn btn-warning btn-lg" href="#">Probar</a>
+        <a class="btn btn-success btn-lg" href="#"><i class="fas fa-download"></i> Descargar</a>
+        <a class="btn btn-info btn-lg" href="#"><i class="fab fa-github"></i> Github</a>
+      </div>
+      <div class="mt-5">
+        <p class="text-muted">Desarrollado por <a href="https://www.youtube.com/@TODOMining" class="text-white">TODO Mining</a>.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require_once INCLUDES.'inc_footer.php'; ?>
